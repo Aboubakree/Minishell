5709974957801c76f0593077f5c4ffd15ce5fac9
Aboubakr Krid <akrid@c3r8p10.1337.ma>
2024-04-27 15:27:59 +0100

    env ready to use

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: akrid <akrid@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 10:18:34 by akrid             #+#    #+#             */
-/*   Updated: 2024/04/27 13:46:45 by akrid            ###   ########.fr       */
+/*   Updated: 2024/04/27 15:26:11 by akrid            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ void    env_add_back(t_envirement **env, t_envirement *node)
     t_envirement *temp;
     
     temp = NULL;
-    if (temp == NULL)
+    if (*env == NULL)
         *env = node;
     else
     {
@@ -48,12 +48,11 @@ void get_envirement(t_envirement **env, char **base_env)
     char *key;
     char *value;
 
-    *env = NULL;
     i = 0;
     while (base_env[i])
     {
         equal = ft_strchr(base_env[i], '=');
-        key = ft_substr(base_env[i], 0, base_env[i] - equal);
+        key = ft_substr(base_env[i], 0, equal - base_env[i]);
         value = ft_substr(equal + 1, 0, ft_strlen(base_env[i]));
         env_add_back(env, env_node(key, value));
         i ++;
@@ -65,28 +64,29 @@ int main(int argc, char **argv, char **e)
     t_envirement *env;
     
     (void)argv;
+    env = NULL;
     if (argc > 1)
         return (1);
     get_envirement(&env, e);
-    // char *str;
-    // while (1)
-    // {
-    //     str = readline("minishell-->$");
-    //     if (str == NULL)
-    //         break;
-    //     if (ft_strncmp("env", str, 10) == 0)
-    //     {
-    //         t_envirement *temp;
-    //         temp = env;
-    //         while (temp)
-    //         {
-    //             printf("%s=%s\n", temp->key, temp->value);
-    //             temp = temp->next;
-    //         }
-    //     }
-    //     else
-    //         printf("Your line : %s\n", str);
-    //     free(str);
-    // }
-    // return (0);
+    char *str;
+    while (1)
+    {
+        str = readline("minishell-->$");
+        if (str == NULL)
+            break;
+        if (ft_strncmp("env", str, 10) == 0)
+        {
+            t_envirement *temp;
+            temp = env;
+            while (temp)
+            {
+                printf("%s=%s\n", temp->key, temp->value);
+                temp = temp->next;
+            }
+        }
+        else
+            printf("Your line : %s\n", str);
+        free(str);
+    }
+    return (0);
 }
