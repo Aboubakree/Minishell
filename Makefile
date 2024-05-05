67bb9921e3951f0a061e7760241b59b159eab784
radouane-tamouss <atamousse.red@gmail.com>
2024-05-05 20:36:54 +0100

    Handle misplaced operators considering quotes

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
+GREEN=\033[0;32m
+NC=\033[0m # No Color
+
+CC = @gcc
 NAME = minishell
 
 CFLAGS = -Wall -Werror -Wextra -g # -lreadline#-fsanitize=address
@@ -21,18 +25,21 @@ all : $(NAME)
 
 $(NAME) : $(OBJ) $(LIBFT)
 	$(CC) $(CFLAGS) -o $@ $^ -lreadline
-
+	@echo "\033[0;32m✅ minishell compiled\033[0m"
 $(LIBFT):
-	make -C $(LIBFT_DIR)
-	make bonus -C $(LIBFT_DIR)
+	@make -C $(LIBFT_DIR)
+	@make bonus -C $(LIBFT_DIR)
+	@echo "\033[0;32m✅ libft compiled\033[0m"
 
 clean :
-	make clean -C $(LIBFT_DIR)
-	rm -f $(OBJ)
+	@make clean -C $(LIBFT_DIR)
+	@rm -f $(OBJ)
+	@echo "\033[0;32m🧹 cleaned\033[0m"
 
 fclean : clean
-	make fclean -C $(LIBFT_DIR)
-	rm -f $(NAME)
+	@make fclean -C $(LIBFT_DIR)
+	@rm -f $(NAME)
+	@echo "\033[0;32m🧹🧹 fcleaned\033[0m"
 
 re : fclean all
 
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: rtamouss <rtamouss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 10:18:34 by akrid             #+#    #+#             */
-/*   Updated: 2024/05/05 14:55:11 by rtamouss         ###   ########.fr       */
+/*   Updated: 2024/05/05 17:8:135:21rtamouss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,34 +54,75 @@ int is_whitespace(char c)
     return (c == ' ' || c == '\t' || c == '\n' || c == '\v' || c == '\f' || c == '\r');
 }
 
-int check_misplaced_operators(const char *str)
+int check_logical_operators(const char *str)
 {
     int i;
-    char *pipe;
     
     i = 0;
+    
     while(str[i])
     {
         while(is_whitespace(str[i]))
             i++;
-        if (str[i] == '|')
-            return (printf("Syntax error : Misplaced pipe\n"), 1);
-        pipe = ft_strrchr(str, '|');
-        while(is_whitespace(*pipe))
-            pipe++;
-        if (*pipe == '\0')
-            return (printf("Syntax error : Misplaced pipe\n"), 1);
+        if (str[i] == '&')
+            return (printf("Syntax error : Misplaced Operator at first of line\n"), 1);
+        while (str[i] && str[i] != '&')
+            i++;
+        if (str[i] == '&')
+        {
+            if (str[i + 1] == '&')
+                return (printf("Error: Logical operators '&&' and '||' are not supported YET.\n"), 1);
+        }
         i++;
     }
-    return (0); 
+    return (0);
+}
+
+void update_nb_quote(char c, int *nb_quote_single, int *nb_quote_double)
+{
+    if (c == '\'')
+        *nb_quote_single += 1;
+    if (c == '\"')
+        *nb_quote_double += 1;
+}
+
+int check_misplaced_operators(const char *str)
+{
+    int i;
+    int nb_quote_single;
+    int nb_quote_double;
+    int expect_operator;
+    
+    i = 0;
+    nb_quote_single = 0;
+    nb_quote_double = 0;
+    expect_operator = 0;
+    if (str[i] == '|')
+        return (printf("Syntax error : Misplaced Operator at first of line\n"), 1);
+    while(str[i])
+    {
+        update_nb_quote(str[i], &nb_quote_single, &nb_quote_double);
+        if (str[i] == '|' && nb_quote_single % 2 == 0 && nb_quote_double % 2 == 0)
+        {
+            if (expect_operator == 1)
+                return (printf("Syntax error : Misplaced Operator\n"), 1);
+            expect_operator = 1;
+        }
+        else if (!is_whitespace(str[i]))
+            expect_operator = 0;
+        i++;
+    }
+    return (printf("Syntax error : Misplaced Operator\n"), expect_operator);
 }
 
 int check_syntax_error(const char *str)
 {
     if (check_unclosed_quotes(str) == 1)
         return (printf("Syntax error : Unclosed quote\n"), 1);
-    // if (check_misplaced_operators(str) == 1)
-    //     return (1);
+    if (check_misplaced_operators(str) == 1)
+        return (1);
+    if (check_logical_operators(str) == 1)
+        return (1);
     return (0);
 }
 
